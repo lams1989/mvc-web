47afd8fea37e2ac7be7cab1e37dc80a21d65a170
José Muñoz Sierra <ing.jose.luis@hotmail.com>
2020-07-24 19:13:55 -0500

    fix / error

--- src/main/java/com/lesson/mvc/mvcweb/controller/ProductController.java
+++ src/main/java/com/lesson/mvc/mvcweb/controller/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
-@RequestMapping("/product")
 public class ProductController {
 
     private final ProductService service;
